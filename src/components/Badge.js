50f929497161c446b3e898bbf408e0b51c5d4d45
alan <alanarostegui@hotmail.com>
2020-04-22 23:53:01 -0500

    feature(badges): add props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import confLogo from '../assets/images/badge-header.svg';
-import './style.css';
+import confLogo from "../assets/images/badge-header.svg";
+import "./style.css";
 
 class Badge extends React.Component {
   render() {
+    const { avatarUrl, firstName, lastName, jobTitle, twitter } = this.props;
     return (
       <div className="Badge">
         <div className="Badge__header">
@@ -12,15 +13,19 @@ class Badge extends React.Component {
         </div>
 
         <div className="Badge__section-name">
-          <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+          <img
+            className="Badge__avatar"
+            src="https://www.gravatar.com/avatar?d=identicon"
+            alt="Avatar"
+          />
           <h1>
-            Alan <br /> Maranto
+            {firstName} <br /> {lastName}
           </h1>
         </div>
 
         <div className="Badge__section-info">
-          <h3>Full Stack Developer</h3>
-          <div>@Alan Maranto</div>
+          <h3>{jobTitle}</h3>
+          <div>@{twitter}</div>
         </div>
 
         <div className="Badge__footer">#DevF</div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
-import Badge from './components/Badge';
-import './global.css';
+import Badge from "./components/Badge";
+import "./global.css";
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(
+  <Badge
+    firstName="Alan"
+    lastName="Maranto"
+    avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+    jobTitle="Full Stack Engineer"
+    twitter="alanmaranto"
+  />,
+  container
+);
