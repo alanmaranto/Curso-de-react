1a7372d0c5f2d9a24414b9b2018c832edb1c16cc
alan <alanarostegui@hotmail.com>
2020-04-22 23:41:43 -0500

    style(badge): bootstrap and global css

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,28 +1,29 @@
 import React from 'react';
 
 import confLogo from '../assets/images/badge-header.svg';
+import './style.css';
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
 
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
           <h1>
             Alan <br /> Maranto
           </h1>
         </div>
 
-        <div>
-          <p>Full Stack Developer</p>
-          <p>@Alan Maranto</p>
+        <div className="Badge__section-info">
+          <h3>Full Stack Developer</h3>
+          <div>@Alan Maranto</div>
         </div>
 
-        <div>#DevF</div>
+        <div className="Badge__footer">#DevF</div>
       </div>
     );
   }
